0f6f4de9185e73adfd4447ff39dc2cf080e84df5
Kaarle Järvinen <117437182+KaarleJ@users.noreply.github.com>
2024-06-04 10:04:36 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "cdn.sanity.io", port: "" },
-      { protocol: "https", hostname: "www.prokoulu.fi", port: "" },
       { hostname: "source.unsplash.com" },
     ],
   },
